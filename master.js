f7fa2fd0f701e5eca29dbfe17992c5848a8fce9b
ybekirov <47565917+ybekirov@users.noreply.github.com>
2019-09-08 16:23:53 -0500

    Update master.js

--- master.js
+++ master.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){$('.sis-app-root').find(\"img\").css(\"height\",\"180px\"); $(\"login-footer\").remove();});
+$(document).ready(function(){$('.sis-app-root').find("img").css("height","180px"); $("login-footer").remove();});
